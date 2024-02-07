f91cabfe0b00aabcffb835c688bbb6f4e359cd68
Manoj Jacob <manjacob@microsoft.com>
2024-02-07 11:06:07 +0530

    update

--- utils/buildContainerTemplate.sh
+++ utils/buildContainerTemplate.sh
@@ -25,6 +25,7 @@ set -euxo pipefail  # fail on error
 # in alphabetical order, then another checksum of their names and checksums.
 # Running this command on windows-based infrastructure may return a different result due to CRLF
 pushd $GITHUB_WORKSPACE/$SRC_FOLDER/$SOURCE_LOCATION
+git version
 imageTag=$(git log -n 1 --format="%H" -- ".")
 popd
 echo "imageTag: $imageTag"  
