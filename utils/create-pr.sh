e383008191f78f0d62c1e986816267acf19ff335
Manoj Jacob <manjacob@microsoft.com>
2024-02-07 21:36:23 +0530

    update

--- utils/create-pr.sh
+++ utils/create-pr.sh
@@ -60,9 +60,8 @@ if [[ `git status --porcelain | head -1` ]]; then
     # Push to the deploy branch 
     echo "Push to the deploy branch $deploy_branch_name"
     echo "git push --set-upstream $repo_url $deploy_branch_name"
-
     git push --set-upstream $repo_url $deploy_branch_name
-
+    echo "============================================="
     # Create a PR 
     echo "Create a PR to $DEST_BRANCH"
     
