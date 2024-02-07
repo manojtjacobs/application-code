20854bdd245ad5381e4a1c0f995cb6b373532b16
Manoj Jacob <manjacob@microsoft.com>
2024-02-07 18:30:36 +0530

    update

--- utils/buildContainerTemplate.sh
+++ utils/buildContainerTemplate.sh
@@ -25,11 +25,9 @@ set -euxo pipefail  # fail on error
 # in alphabetical order, then another checksum of their names and checksums.
 # Running this command on windows-based infrastructure may return a different result due to CRLF
 pushd $GITHUB_WORKSPACE/$SRC_FOLDER/$SOURCE_LOCATION
-git --version
+
 imageTag=$(git log -n 1 --format="%H" -- ".")
 popd
-echo "imageTag: $imageTag" 
-az account show 
 # If the image with the generated tag doesn't already exist, build it.
 if ! az acr repository show -n $AZ_ACR_NAME --image "$REPOSITORY:$imageTag" -o table; then
     echo No match found. Container will be built.
